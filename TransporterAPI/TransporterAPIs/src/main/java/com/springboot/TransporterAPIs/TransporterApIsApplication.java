a85c60fd62dd5895aac06b5c0a823cd2a67e3ce7
PRANAV_GUPTA <16ucs132@lnmiit.ac.in>
2021-04-14 02:17:45 +0530

    testing_purpose

--- TransporterAPI/TransporterAPIs/src/main/java/com/springboot/TransporterAPIs/TransporterApIsApplication.java
+++ TransporterAPI/TransporterAPIs/src/main/java/com/springboot/TransporterAPIs/TransporterApIsApplication.java
@@ -8,6 +8,7 @@ public class TransporterApIsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransporterApIsApplication.class, args);
+		System.out.print("this is for testing");
 	}
 
 }
